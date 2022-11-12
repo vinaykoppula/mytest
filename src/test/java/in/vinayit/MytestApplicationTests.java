45d55344627b40959b26cf828fadc8d86f109469
vinaykoppula <vinaykoppula999@gmail.com>
2022-11-11 20:25:09 -0600

    Initial Commit

---
+++ src/test/java/in/vinayit/MytestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vinayit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

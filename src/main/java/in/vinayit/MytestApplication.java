44a1b11d859a969fd6d6f721284211abde27569b
vinaykoppula <vinaykoppula999@gmail.com>
2022-11-11 20:33:01 -0600

    Main class modified

--- src/main/java/in/vinayit/MytestApplication.java
+++ src/main/java/in/vinayit/MytestApplication.java
@@ -2,12 +2,14 @@ package in.vinayit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MytestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MytestApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(MytestApplication.class, args);
+		run.close();
 	}
 
 }

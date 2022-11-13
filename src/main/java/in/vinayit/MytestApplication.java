698abc5e887aecf3839069fb6b7c33148383cead
vinaykoppula <vinaykoppula999@gmail.com>
2022-11-12 21:00:14 -0600

    removes closing method for run object

--- src/main/java/in/vinayit/MytestApplication.java
+++ src/main/java/in/vinayit/MytestApplication.java
@@ -10,7 +10,7 @@ public class MytestApplication {
 	public static void main(String[] args) {
 		//Changes for HIS-134
 		ConfigurableApplicationContext run = SpringApplication.run(MytestApplication.class, args);
-		run.close();
+		
 	}
 
 }
